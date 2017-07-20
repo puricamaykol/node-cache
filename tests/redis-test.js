7423b53ebb95eef5c1c551a417c4ea1e3829bca8
puricamaykol <puricamaykol@gmail.com>
2017-07-20 18:58:29 -0400

    modified to use default port

--- tests/redis-test.js
+++ tests/redis-test.js
@@ -8,13 +8,13 @@ var redis = require('../lib/Estrategies/RedisAdapter');
 describe("#Redis", function() {
 	describe("#get( ) method", function() {
 		it("Should get a value by given key if the key exists", function() {
-			let client = new redis("127.0.0.1", 6379);
+			let client = new redis("127.0.0.1");
 			client.set('TestGETKEY', "some_value");
 
 			return client.get('TestGETKEY').should.eventually.deep.equal("some_value");
     	});
     	it("Should reject the promise if given key doesn't exist", function() {
-			let client = new redis("127.0.0.1", 6379);
+			let client = new redis("127.0.0.1");
 			//client.delete('TestGETKEYNotCreated');
 
 			return client.get('TestGETKEYNotCreated').should.be.rejectedWith("The provided key doesn't exist!"); 
@@ -23,16 +23,16 @@ describe("#Redis", function() {
 	});
 	describe("#set( ) method", function() {
 		afterEach(function() {
-        let setUpClient = new redis("127.0.0.1", 6379);
+        let setUpClient = new redis("127.0.0.1");
 			 setUpClient.delete('TestsetKEY');
     		
   		});
 		it("Should return OK if succeeded", function() {
-			let client = new redis("127.0.0.1", 6379);
+			let client = new redis("127.0.0.1");
 			return client.set('TestsetKEY', "some_value_set").should.eventually.deep.equal("OK");
     	});
     	it("Should store the value with the given key", function() {
-			let client = new redis("127.0.0.1", 6379);
+			let client = new redis("127.0.0.1");
 			 client.set('TestsetKEY', "some_value_set_two");
 			client.get('TestsetKEY').should.eventually.deep.equal("some_value_set_two");
     	});
@@ -40,7 +40,7 @@ describe("#Redis", function() {
 	});
 	describe("#getHash( ) method", function(){
 			it("Should get a hash (as an Object literall) by given key if the key exists", function() {
-			let client = new redis("127.0.0.1", 6379);
+			let client = new redis("127.0.0.1");
 			client.setHash('TestGETHASHKEY', {"attr1": "vl1", "atrr2":"val2"});
 
 			return client.getHash('TestGETHASHKEY').should.eventually.deep.equal({"attr1": "vl1", "atrr2":"val2"});
@@ -48,16 +48,16 @@ describe("#Redis", function() {
 	});
 	describe("#setHash( ) method", function() {
 		afterEach(function() {
-        let setUpClient = new redis("127.0.0.1", 6379);
+        let setUpClient = new redis("127.0.0.1");
 			 setUpClient.delete('TestsetHashKEY');
     		
   		});
 		it("Should return OK if succeeded", function() {
-			let client = new redis("127.0.0.1", 6379);
+			let client = new redis("127.0.0.1");
 			return client.setHash('TestsetHashKEY', "some_value_set").should.eventually.deep.equal("OK");
     	});
     	it("Should store the object with the given key", function() {
-			let client = new redis("127.0.0.1", 6379);
+			let client = new redis("127.0.0.1");
 			 client.setHash('TestsetHashKEY', {"attr1": "val1", "attr2":"val2"});
 			 client.getHash('TestsetHashKEY').should.eventually.deep.equal({"attr1": "val1", "attr2":"val2"});
 			 client.getHash('TestsetHashKEY').should.eventually.have.property("attr1", "val1");
@@ -67,12 +67,12 @@ describe("#Redis", function() {
 	});
 	describe("#delete( ) method", function(){
 		it("Should return OK if succeeded", function() {
-			let client = new redis("127.0.0.1", 6379);
+			let client = new redis("127.0.0.1");
 			 client.set('testDelkey', "some_value_to_del");
 			 client.delete('testDelkey').should.eventually.deep.equal("OK");
     	});
     	it("Should be rejected if KEY doesn't exist", function() {
-			let client = new redis("127.0.0.1", 6379);
+			let client = new redis("127.0.0.1");
 			 client.delete('testDelkeyDE').should.be.rejectedWith("The provided key doesn't exist!"); 
     	});
 	});
