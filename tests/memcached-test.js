b91aafc55b6d4c860495536ad43b15bb86bb064c
puricamaykol <puricamaykol@gmail.com>
2017-07-30 18:56:12 -0400

    mock memcached service methods

--- tests/memcached-test.js
+++ tests/memcached-test.js
@@ -6,75 +6,138 @@ chai.should();
 
 var memcached = require('../lib/Estrategies/MemcachedAdapter');
 describe("#Memcached", function() {
-	describe("#get( ) method", function() {
-		it("Should get a value by given key if the key exists", function() {
-			let client = new memcached("127.0.0.1", 11211);
-			return client.set('TestMCGETKEY', "some_value").then(rep=>client.get('TestMCGETKEY').should.eventually.deep.equal("some_value"));
+  describe("#get( ) method", function() {
+    it("Should get a value by given key if the key exists", function() {
+      var memcachedThirdparty = {
+        get: function(key, cb) {
+          var cb = cb;
+          cb(null, "some_value");
+        }
+      };
+      let client = new memcached("localhost", 6379, memcachedThirdparty);
 
-			
-    	});
-    	it("Should reject the promise if given key doesn't exist", function() {
-			let client = new memcached("127.0.0.1", 11211);
-			//client.delete('TestGETKEYNotCreated');
+      return client.get('TestGETKEY').should.eventually.deep.equal("some_value");
+    });
+    it("Should reject the promise if given key doesn't exist", function() {
+      var memcachedThirdparty = {
+        get: function(key, cb) {
+          var cb = cb;
+          cb("The provided key doesn't exist!", null);
+        }
+      };
+      let client = new memcached("localhost", 6379, memcachedThirdparty);
 
-			return client.get('TestMCGETKEYNotCreated').should.be.rejectedWith("The provided key doesn't exist!"); 
+      return client.get('TestGETKEYNotCreated').should.be.rejectedWith("The provided key doesn't exist!");
 
-    	});
-	});
-	describe("#set( ) method", function() {
-		afterEach(function() {
-        let setUpClient = new memcached("127.0.0.1", 11211);
-			 setUpClient.delete('TestsetKEY');
-    		
-  		});
-		it("Should return OK if succeeded", function() {
-			let client = new memcached("127.0.0.1", 11211);
-			return client.set('TestsetKEY', "some_value_set").should.eventually.deep.equal("OK");
-    	});
-    	it("Should store the value with the given key", function() {
-			let client = new memcached("127.0.0.1", 11211);
-			 client.set('TestsetKEY', "some_value_set_two");
-			client.get('TestsetKEY').should.eventually.deep.equal("some_value_set_two");
-    	});
+    });
+  });
+  describe("#set( ) method", function() {
 
-	});
-	describe("#getHash( ) method", function(){
-			it("Should get a hash (as an Object literall) by given key if the key exists", function() {
-			let client = new memcached("127.0.0.1", 11211);
-			client.setHash('TestGETHASHKEY', {"attr1": "vl1", "atrr2":"val2"});
+    it("Should store the value with the given key", function() {
+      var memcachedThirdparty = {
+        set: function(key, val, time,  cb) {
+          var cb = cb;
+          cb(null, "OK");
+        }
+      };
+      let client = new memcached("localhost", 6379, memcachedThirdparty);
+      return client.set('TestsetKEY', "some_value_set").should.eventually.deep.equal("OK");
+    });
+    it("Should reject the promise if an error occurs", function() {
+      var memcachedThirdparty = {
+        set: function(key, val, time, cb) {
+          var cb = cb;
+          cb("There was an error storing the key", null);
+        }
+      };
+      let client = new memcached("localhost", 6379, memcachedThirdparty);
+      return client.set('TestsetKEY', "some_value_set").should.be.rejectedWith("There was an error storing the key");
+    });
 
-			return client.getHash('TestGETHASHKEY').should.eventually.deep.equal({"attr1": "vl1", "atrr2":"val2"});
-    	});
-	});
-	describe("#setHash( ) method", function() {
-		afterEach(function() {
-        let setUpClient = new memcached("127.0.0.1", 11211);
-			 setUpClient.delete('TestsetHashKEY');
-    		
-  		});
-		it("Should return OK if succeeded", function() {
-			let client = new memcached("127.0.0.1", 11211);
-			return client.setHash('TestsetHashKEY', "some_value_set").should.eventually.deep.equal("OK");
-    	});
-    	it("Should store the object with the given key", function() {
-			let client = new memcached("127.0.0.1", 11211);
-			 client.setHash('TestsetHashKEY', {"attr1": "val1", "attr2":"val2"});
-			 client.getHash('TestsetHashKEY').should.eventually.deep.equal({"attr1": "val1", "attr2":"val2"});
-			 client.getHash('TestsetHashKEY').should.eventually.have.property("attr1", "val1");
-			 client.getHash('TestsetHashKEY').should.eventually.have.property("attr2", "val2");
-    	});
+  });
+  describe("#getHash( ) method", function() {
+    it("Should get a hash (as an Object literall) by given key if the key exists", function() {
+      var memcachedThirdparty = {
+        get: function(key, cb) {
+          var cb = cb;
+          cb(null, {
+            "attr1": "vl1",
+            "atrr2": "val2"
+          });
+        }
+      };
+      let client = new memcached("localhost", 6379, memcachedThirdparty);
+      return client.getHash('TestGETHASHKEY').should.eventually.deep.equal({
+        "attr1": "vl1",
+        "atrr2": "val2"
+      });
+    });
+    it("Should reject the promise if error occurs", function() {
+      var memcachedThirdparty = {
+        get: function(key, cb) {
+          var cb = cb;
+          cb("There was an error", null);
+        }
+      };
+      let client = new memcached("localhost", 6379, memcachedThirdparty);
 
-	});
-	describe("#delete( ) method", function(){
-		it("Should return OK if succeeded", function() {
-			let client = new memcached("127.0.0.1", 11211);
-			 client.set('testDelkey', "some_value_to_del");
-			 client.delete('testDelkey').should.eventually.deep.equal("OK");
-    	});
-    	it("Should be rejected if KEY doesn't exist", function() {
-			let client = new memcached("127.0.0.1", 11211);
-			 client.delete('testDelkeyDE').should.be.rejectedWith("The provided key doesn't exist!"); 
-    	});
-	});
+      return client.getHash('TestGETKEYNotCreated').should.be.rejectedWith("There was an error");
 
+    });
+  });
+  describe("#setHash( ) method", function() {
+    it("Should store the object with the given key", function() {
+      var memcachedThirdparty = {
+        set: function(key, val, time,  cb) {
+          var cb = cb;
+          cb(null, "OK");
+        }
+      };
+      let client = new memcached("localhost", 6379, memcachedThirdparty);
+      return client.setHash('TestsetHashKEY', "some_value_set").should.eventually.deep.equal("OK");
+    });
+
+    it("Should reject the promise if an error occurs", function() {
+      var memcachedThirdparty = {
+        set: function(key, val, time, cb) {
+          var cb = cb;
+          cb("There was an error storing the key", null);
+        }
+      };
+      let client = new memcached("localhost", 6379, memcachedThirdparty);
+      return client.setHash('TestsetHashKEY', "some_value_set").should.be.rejectedWith("There was an error storing the key");
+    });
+
+  });
+  describe("#delete( ) method", function() {
+    it("Should return OK if succeeded", function() {
+      var memcachedThirdparty = {
+        del: function(key, cb) {
+          var cb = cb;
+          cb(null, 1);
+        },
+        get: function(key, cb) {
+          var cb = cb;
+          cb(null, "some_value");
+        }
+      };
+      let client = new memcached("localhost", 6379, memcachedThirdparty);
+      return client.delete('testDelkey').should.eventually.deep.equal("OK");
+    });
+    
+    it("Should reject the promise if an error occurs", function() {
+      var memcachedThirdparty = {
+        del: function(key, cb) {
+          var cb = cb;
+          cb("There was an error deleting", null);
+        },
+        get: function(key, cb) {
+          var cb = cb;
+          cb(null, "some_value");
+        }
+      };
+      let client = new memcached("localhost", 6379, memcachedThirdparty);
+      return client.delete('TestsetHashKEY').should.be.rejectedWith("There was an error deleting");
+    });
+  });
 });
